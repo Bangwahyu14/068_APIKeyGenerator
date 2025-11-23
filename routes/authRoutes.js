8a4ed53f496f235ce01ee413d89ff0224489ed9d
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-23 22:16:41 +0700

    memperbaiki authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// Halaman login dan register (HTML statis)
+// Halaman login + register
 router.get('/login', (req, res) => {
   res.sendFile('login.html', { root: './public/html' });
 });
+
 router.get('/register', (req, res) => {
   res.sendFile('register.html', { root: './public/html' });
 });
 
-// Aksi register dan login (POST)
-router.post('/register', authController.registerUser);
+// Login & Register action
 router.post('/login', authController.loginUser);
+router.post('/register', authController.registerUser);
 
 module.exports = router;
